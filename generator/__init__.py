22e5b6199d5aea3d2055efec3dcd8e70b6ca472d
Högni Gylfason <hognig@vis.is>
2013-05-11 19:52:31 +0000

    generator package

---
+++ generator/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+everybody_dies.generator
+========================
+
+This package contains the main generator classes
+"""
+from __future__ import unicode_literals, print_function, absolute_import
