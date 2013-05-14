bda81ad74b686f46b8a9b61a1a16ec50cc36f502
Högni Gylfason <hognig@vis.is>
2013-05-14 18:08:14 +0000

    Added border lists to Room

--- generator/room.py
+++ generator/room.py
@@ -37,10 +37,24 @@ class Room:
         return "<Room(%(id)s), pos(%(pos)s) W: %(width)s, H: %(height)s>" % \
             self.__dict__
 
+    def get_connected_rooms(self):
+        # Get adjoining rooms
+
+        # Traverse hallways to connected rooms
+        pass
+
+    @property
+    def top_border(self):
+        return [[p[0], p[1] - 1] for p in self.top_edge]
+
     @property
     def top_edge(self):
         return [[x, self.pos[1]]
                 for x in range(self.pos[0], self.pos[0] + self.width)]
+
+    @property
+    def bottom_border(self):
+        return [[p[0], p[1] + 1] for p in self.bottom_edge]
 
     @property
     def bottom_edge(self):
@@ -48,9 +62,17 @@ class Room:
                 for x in range(self.pos[0], self.pos[0] + self.width)]
 
     @property
+    def left_border(self):
+        return [[p[0] - 1, p[1]] for p in self.left_edge]
+
+    @property
     def left_edge(self):
         return [[self.pos[0], y]
                 for y in range(self.pos[1], self.pos[1] + self.height)]
+
+    @property
+    def right_border(self):
+        return [[p[0] + 1, p[1]] for p in self.right_edge]
 
     @property
     def right_edge(self):
